2adef56bf21c07218d349da0f763981de3e5585c
Collin Erickson <collinberickson@gmail.com>
2016-07-28 17:11:40 -0700

    Got joint MLE with theta and nug working, gradient works
    MLE needs work on error catching and number of starts

--- R/GauPro.R
+++ R/GauPro.R
@@ -7,6 +7,7 @@ GauPro <- R6Class(classname = "GauPro",
     D = NA,
     corr = "Gauss",
     nug = 1e-6,
+    nug.min = 1e-8,
     theta = 1,
     mu_hat = NA,
     s2_hat = NA,
@@ -19,10 +20,11 @@ GauPro <- R6Class(classname = "GauPro",
       self$N <- nrow(X)
       self$D <- ncol(X)
       #self$update_params()
-      self$theta_update()
+      #self$theta_update()
+      self$all_update()
     },
-    update_params = function () {
-      self$K <- gauss_cor_mat(self$X, theta=self$theta) + diag(self$nug, self$N)
+    update_params = function () {#browser()
+      self$K <- corr_gauss_mat(self$X, theta=self$theta) + diag(self$nug, self$N)
       self$Kchol <- chol(self$K)
       self$Kinv <- chol2inv(self$Kchol)
       self$mu_hat <- sum(self$Kinv %*% self$Z) / sum(self$Kinv)
@@ -36,8 +38,8 @@ GauPro <- R6Class(classname = "GauPro",
       }
       #covmat <- gauss_cor(c(x, xx))
       #kx <- gauss_cor_mat(self$X) + diag(self$nug, self$N)
-      kxx <- gauss_cor_mat(XX, theta=self$theta)
-      kx.xx <- gauss_cor_mat(XX, self$X, theta=self$theta)
+      kxx <- corr_gauss_mat(XX, theta=self$theta)
+      kx.xx <- corr_gauss_mat(XX, self$X, theta=self$theta)
 
       mn <- self$pred_mean(XX, kx.xx=kx.xx)#self$mu_hat + kx.xx %*% self$Kinv %*% (self$Z - self$mu_hat)
       #s2 <- self$s2_hat * (1 - kx.xx %*% self$Kinv %*% t(kx.xx) + (1-sum(self$Kinv %*% t(kx.xx)))^2 / (sum(self$Kinv)))
@@ -54,7 +56,7 @@ GauPro <- R6Class(classname = "GauPro",
       self$s2_hat * diag(kxx - kx.xx %*% self$Kinv %*% t(kx.xx))
     },
     deviance = function (theta) {
-      K <- gauss_cor_mat(self$X, theta=theta) + diag(self$nug, self$N)
+      K <- corr_gauss_mat(self$X, theta=theta) + diag(self$nug, self$N)
       Kchol <- chol(K)
       Kinv <- chol2inv(Kchol)
       mu_hat <- sum(Kinv %*% self$Z) / sum(Kinv)
@@ -100,6 +102,131 @@ GauPro <- R6Class(classname = "GauPro",
       plot(x,px$me, type='l', lwd=4, ylim=c(min(newy),max(newy)))
       sapply(1:n2, function(i) points(x, newy[i,], type='l', col='gray'))
       points(self$X, self$Z, pch=19, col=1, cex=2)
+    },
+
+
+
+
+    deviance2 = function (theta, nug) { # joint deviance
+      K <- corr_gauss_mat(self$X, theta=theta) + diag(nug, self$N)
+      Kchol <- try(chol(K))
+      if (inherits(Kchol, "try-error")) {return(Inf)}
+      Kinv <- chol2inv(Kchol)
+      mu_hat <- sum(Kinv %*% self$Z) / sum(Kinv)
+      s2_hat <- c(t(self$Z - self$mu_hat) %*% Kinv %*% (self$Z - mu_hat) / self$N)
+      detK <- prod(diag(Kchol))^2
+      log(detK) + self$N * log((self$Z - mu_hat) %*% solve(K, self$Z - mu_hat))
+    },
+    deviance_log2 = function (params) { # joint deviance
+      beta <- params[-length(params)]
+      nug <- params[length(params)]
+      theta <- 10^beta
+      self$deviance2(theta=theta, nug=nug)
+    },
+    deviance_search2 = function () {
+      # Joint MLE deviance search with genetic alg, slow and bad
+      #optimize(self$deviance_log, interval = c(-10,10), maximum = F, tol=1e-3)
+      nvars <- self$D
+      suppressWarnings(
+        rgenoud::genoud(self$deviance_log2, nvars=nvars + 1,#self$D,#1,
+                        Domains = rbind(matrix(c(-10,10), nvars, 2, byrow=T),c(0,1)), max = F,
+                        solution.tolerance=1e-3,
+                        pop.size=min(1000, 200*self$D),
+                        max.generations = min(10, 3*self$D),
+                        print.level = 0
+        )
+      )
+    },
+    deviance_search3 = function (restarts = 5) {#browser()
+      # Joint MLE search with L-BFGS-B, with restarts
+      #optimize(self$deviance_log, interval = c(-10,10), maximum = F, tol=1e-3)
+      #nvars <- self$D
+      lower <- c(rep(-10, self$D), self$nug.min)
+      upper <- c(rep(10, self$D), Inf)
+      # maybe start by calculating deviance for current params
+      best <- try(
+        optim(c(log(self$theta, 10),self$nug), self$deviance_log2, method="L-BFGS-B", lower=lower, upper=upper, hessian=F)
+      )
+      if (inherits(best, "try-error")) {
+        best <- list(par=c(log(self$theta, 10), self$nug), value = self$deviance_log2(c(log(self$theta, 10))))
+      }
+      if (restarts >= 1) {
+        for (i in 1:restarts) {
+          current <- try(
+            optim(c(log(self$theta, 10) + rnorm(self$D,0,2),self$nug + rexp(1, 1e4)), self$deviance_log2, method="L-BFGS-B", lower=lower, upper=upper, hessian=F)
+          )
+          if (!inherits(current, "try-error")) {
+            if (current$value < best$value) {
+              best <- current
+            }
+          }
+        }
+      }
+      best
+    },
+    all_update = function () { # update theta and nugget
+      pars <- self$deviance_search3()$par
+      #self$theta <- 10 ^ self$deviance_search()$minimum
+      self$nug <- pars[length(pars)]
+      self$theta <- 10 ^ pars[-length(pars)]
+      self$update_params()
+    },
+
+    deviance_searchnug = function() {
+      optim(self$nug, function(nnug) {self$deviance2(theta=self$theta, nug=nnug)}, method="L-BFGS-B", lower=0, upper=Inf, hessian=F)$par
+    },
+    nugget_update = function () {
+      nug <- self$deviance_searchnug()
+      #self$theta <- 10 ^ self$deviance_search()$minimum
+      self$nug <- nug
+      self$update_params()
+    },
+    mean_grad = function (XX) {#browser()
+
+      if (!is.matrix(XX)) {
+        if (self$D == 1) XX <- matrix(XX, ncol=1)
+        else if (length(XX) == self$D) XX <- matrix(XX, nrow=1)
+        else stop('Predict input should be matrix')
+      } else {
+        if (ncol(XX) != self$D) {stop("Wrong dimension input")}
+      }
+      kx.xx <- t(corr_gauss_mat(XX, self$X, theta=self$theta))
+
+      #grad <- numeric(self$D)
+      #drdx <- matrix(NA, self$N, self$D)
+      #for (i in 1:self$N) {
+      #  for (j in 1:self$D) {
+      #    drdx[i, j] <- -2 * self$theta[j] * (XX[j, 1] - self$X[i, j]) * kx.xx[i, 1]
+      #  }
+      #}
+      #drdx <- -2 * outer(1:self$N, 1:self$D, Vectorize(function(i,j) {self$theta[j] * (XX[j, 1] - self$X[i, j]) * kx.xx[i, 1]}))
+      #t(drdx) %*% self$Kinv %*% (self$Z - self$mu_hat)
+
+      # mult points and dims
+      #drdx <- vapply(1:nrow(XX), Vectorize(function(k) -2 * outer(1:self$N, 1:self$D, Vectorize(function(i,j) {self$theta[j] * (XX[k, j] - self$X[i, j]) * kx.xx[i, k]}))), numeric(self$N))
+      #t(drdx) %*% self$Kinv %*% (self$Z - self$mu_hat)
+
+      grad <-   vapply(1:nrow(XX),
+
+                      Vectorize(
+                        function(k) {
+                          t(-2 * outer(1:self$N, 1:self$D, Vectorize(function(i,j) {self$theta[j] * (XX[k, j] - self$X[i, j]) * kx.xx[i, k]}))
+                          )  %*%self$Kinv %*% (self$Z - self$mu_hat)
+                        }
+                      )
+
+               #, matrix(0, self$N, nrow(XX)) #rep(0, self$N)
+               , numeric(self$D) #rep(0, self$N)
+
+        )
+      if (self$D == 1) return(grad)
+      t(grad)
+
+    },
+    mean_grad_norm = function (XX) {#browser()
+      grad <- self$mean_grad(XX)
+      if (!is.matrix(grad)) return(abs(grad))
+      apply(grad,1, function(xx) {sqrt(sum(xx^2))})
     }
   ),
   private = list(
--- tests/test1.R
+++ tests/test1.R
@@ -6,22 +6,22 @@ gp$pred(matrix(runif(6),3,2))
 # 1D test
 n <- 12
 x <- matrix(seq(0,1,length.out = 12), ncol=1)
-y <- sin(2*pi*x) + rnorm(n,0,1e-1)
+y <- sin(2*pi*x) #+ rnorm(n,0,1e-1)
 y <- sqrt(x)-x
 y <- (2*x) %%1
 y <- c(y)
 plot(x,y)
 gp <- GauPro$new()
 gp$fit(x,y)
-gp$cool1Dplot()
-gp$pred(matrix(runif(6),6,1))
+#gp$cool1Dplot()
+#gp$pred(matrix(runif(6),6,1))
 curve(gp$pred(x)$me);points(x,y)
 curve(gp$pred(x)$me+2*gp$pred(x)$se,col=2,add=T);curve(gp$pred(x)$me-2*gp$pred(x)$se,col=2,add=T)
 #gpf <- UGP::UGP$new(X=x,Z=y, package="mlegp")
 #gpf$mod[[1]]$mu;gpf$mod[[1]]$beta;gpf$mod[[1]]$nug
 #gp$mu_hat
 # deviance test
-curve(sapply(x, gp$deviance_log),-10,10)
+curve(sapply(x, gp$deviance_log),-10,10, n = 300)
 gp$deviance_search()
 
 gp$theta
@@ -29,19 +29,68 @@ gp$deviance_search()
 gp$theta_update()
 gp$theta
 
+gp$deviance_search3()
+c(gp$theta,gp$nug)
+gp$all_update()
+c(gp$theta,gp$nug)
+
 gp <- GauPro$new();gp$fit(x,y);gp$cool1Dplot()
+
+# gradtest
+gp <- GauPro$new()
+gp$fit(x,y)
+gp$mean_grad(c(.5,.6))
+par(mfrow=c(2,1))
+curve(gp$pred(x)$me);points(x,y)
+curve(gp$pred(x)$me+2*gp$pred(x)$se,col=2,add=T);curve(gp$pred(x)$me-2*gp$pred(x)$se,col=2,add=T)
+curve(gp$mean_grad(x), add=F, col=3)
+abline(v=x)
+curve(gp$mean_grad_norm(x))
+gp$mean_grad_norm(c(.5,.6))
 
 # 2D test
 n <- 40
 x <- matrix(runif(n*2), ncol=2)
-f1 <- function(a) {sin(1*pi*a[1]) + sin(6*pi*a[2])}
+f1 <- function(a) {sin(3*pi*a[1]) + sin(3*pi*a[2])}
 y <- apply(x,1,f1)
 system.time(contourfilled::contourfilled.data(x,y))
 gp <- GauPro$new()
-gp$fit(x,y)
+gp$fit(x,y);gp$theta
 gp$pred(matrix(runif(6),3,2))
-rbind(gp$mu_hat, gp$s2_hat)
+c(gp$mu_hat, gp$s2_hat)
 system.time(contourfilled::contourfilled.func(gp$pred,out.name = 'mean', pts=x))
 plot(y,gp$pred(x)$mean);abline(a=0,b=1)
+system.time(print(gp$deviance_search()))
+system.time(print(gp$deviance_search3()))
+c(gp$theta, gp$nug)
+gp$all_update()
+c(gp$theta, gp$nug)
+system.time(contourfilled::contourfilled.func(gp$pred,out.name = 'mean', pts=x))
+
+gp$mean_grad(matrix(c(.5, .75, .5, .83),2,2,byrow=T))
+gp$mean_grad(matrix(c(.5, .75),1,2,byrow=T))
+contourfilled::contourfilled.func(function(xx){gp$mean_grad(xx)[2]})
+gp$mean_grad_norm(matrix(c(.5, .75, .5, .83),2,2,byrow=T))
+gp$mean_grad_norm(c(.5,.5))
+contourfilled::contourfilled.func(function(xx){gp$mean_grad_norm(xx)})
+
+
 gpf <- UGP::UGP$new(X=x,Z=y, package="mlegp")
 gpf$mod[[1]]
+
+
+
+
+# higher dim test
+n <- 60
+d <- 3
+x <- matrix(runif(n*d), ncol=d)
+f1 <- function(a) {sum(sin(1:d*pi*a))}
+y <- apply(x,1,f1)
+gp <- GauPro$new()
+gp$fit(x,y);c(gp$theta,gp$nug)
+nn <- 20
+gp$pred(matrix(runif(nn*d),ncol=d))
+gp$mean_grad(matrix(runif(nn*d),ncol=d))
+gp$mean_grad_norm(matrix(runif(nn*d),ncol=d))
+plot(y,gp$pred(x)$mean);abline(a=0,b=1)
