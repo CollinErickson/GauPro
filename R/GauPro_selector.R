2c4a729c86e4fdd1121a3903ef4ef74823980be7
Collin Erickson <collinberickson@gmail.com>
2017-03-04 22:13:09 -0600

    Trying to pass Travis

--- R/GauPro_selector.R
+++ R/GauPro_selector.R
@@ -11,7 +11,7 @@
 #' x <- matrix(seq(0,1,length.out = n), ncol=1)
 #' #y <- sin(2*pi*x) + rnorm(n,0,1e-1)
 #' y <- (2*x) %%1
-#' gp <- GauPro(X=x, Z=y)
+#' gp <- GauPro(X=x, Z=y, parallel=FALSE)
 GauPro <- function(..., type="Gauss") {
   gp <- GauPro_Gauss$new(...)
   return(gp)
