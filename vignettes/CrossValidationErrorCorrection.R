d2b7cd4e8f584b293308ca37d5cf9d64ad1c45de
Collin Erickson <collinberickson@gmail.com>
2017-06-17 19:14:03 -0400

    Built vignette

---
+++ vignettes/CrossValidationErrorCorrection.R
@@ -0,0 +1,23 @@
+## ------------------------------------------------------------------------
+n <- 200
+m1 <- matrix(runif(n*n),ncol=n)
+b1 <- runif(n)
+microbenchmark::microbenchmark(solve(m1, b1), m1 %*% b1)
+
+## ------------------------------------------------------------------------
+set.seed(0)
+corr <- function(x,y) {exp(sum(-30*(x-y)^2))}
+n <- 200
+d <- 2
+X <- matrix(runif(n*d),ncol=2)
+R <- outer(1:n,1:n, Vectorize(function(i,j) {corr(X[i,], X[j,])}))
+Rinv <- solve(R)
+A <- R[-n,-n]
+Ainv <- solve(A)
+E <- Rinv[-n, -n]
+b <- R[n,-n]
+g <- Rinv[n,-n]
+Ainv_shortcut <- E + E %*% b %*% g / (1-sum(g*b))
+summary(c(Ainv - Ainv_shortcut))
+microbenchmark::microbenchmark(solve(A), E + E %*% b %*% g / (1-sum(g*b)))
+
