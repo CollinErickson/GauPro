f4c2b03d9ded0dca7e966674c1372a8786bb08f3
Collin Erickson <collinberickson@gmail.com>
2017-06-28 21:45:47 -0400

    Added S3 for plot

--- R/GauPro_S3.R
+++ R/GauPro_S3.R
@@ -20,6 +20,28 @@ predict.GauPro <- function(object, XX, se.fit=F, covmat=F, split_speed=T, ...) {
   object$predict(XX=XX, se.fit=se.fit, covmat=covmat, split_speed=split_speed)
 }
 
+#' Plot for class GauPro
+#'
+#' @param x Object of class GauPro
+#' @param ... Additional parameters
+#'
+#' @return Nothing
+#' @export
+#'
+#' @examples
+#' n <- 12
+#' x <- matrix(seq(0,1,length.out = n), ncol=1)
+#' y <- sin(2*pi*x) + rnorm(n,0,1e-1)
+#' gp <- GauPro(X=x, Z=y, parallel=FALSE)
+#' plot(gp)
+plot.GauPro <- function(x,  ...) {
+  if (x$D == 1) {
+    x$cool1Dplot(...)
+  } else {
+    stop("No plot method for higher than 1 dimension")
+  }
+}
+
 
 #' Kernel sum
 #'
