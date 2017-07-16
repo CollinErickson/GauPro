21e821dc9b30dfd194d8f4e5c3bb417d64906c69
Collin Erickson <collinberickson@gmail.com>
2017-07-16 08:24:42 -0400

    Added Gaussian_l scratch

---
+++ scratch/scratch_kernel_Gaussian_l.R
@@ -0,0 +1,33 @@
+# Check numerically that gradient is correct for 1D
+# For Gaussian_beta kernel
+set.seed(0)
+n <- 20
+x <- matrix(seq(0,1,length.out = n), ncol=1)
+f <- Vectorize(function(x) {sin(2*pi*x) + .5*sin(4*pi*x) +rnorm(1,0,.3)})
+y <- f(x) #sin(2*pi*x) #+ rnorm(n,0,1e-1)
+gp <- GauPro_kernel_model$new(X=x, Z=y, kernel=Gaussian_l$new(1), parallel=FALSE, verbose=10, nug.est=T)
+gp$cool1Dplot()
+numDeriv::grad(func = function(x) gp$deviance(params=x[1:2], nuglog=x[3]), x=c(2,1, -6))
+gp$deviance_grad(params = c(2,1), nug.update=T, nuglog=-6)
+
+# Check dC_dtheta
+m1 <- (gp$kernel$k(gp$X, l=1+1e-6) - gp$kernel$k(gp$X, l=1-1e-6)) / 1e-6 / 2
+C_nonug <- gp$kernel$k(gp$X, l=1)
+C <- C_nonug + diag(gp$nug, nrow(C_nonug))
+m2 <- gp$kernel$dC_dparams(params = c(0, 1), X = gp$X, C = C, C_nonug = C)[[1]][[1]]
+c(m1 * log(10) -m2) %>% summary
+
+
+
+
+# Check 2D
+set.seed(0)
+n <- 30
+x <- lhs::maximinLHS(n=n, k=2)
+f <- function(x) {sin(2*pi*x[1]) + .5*sin(4*pi*x[1]) +rnorm(1,0,.03) + x[2]^2}
+y <- apply(x, 1, f) #f(x) #sin(2*pi*x) #+ rnorm(n,0,1e-1)
+gp <- GauPro_kernel_model$new(X=x, Z=y, kernel=Gaussian_l$new(c(1, 1)), parallel=FALSE, verbose=10, nug.est=T)
+ContourFunctions::cf(gp$predict, pts=x)
+ContourFunctions::cf(f, pts=x)
+numDeriv::grad(func = function(x)gp$deviance(params = x[1:3], nuglog=x[4]), x=c(1,1, 1, -4))
+gp$deviance_grad(params = c(1,1,1), nug.update=T, nuglog=-4)
