47128209833154c22425716acd3e1a799ba27d55
Collin Erickson <collinberickson@gmail.com>
2017-07-20 19:56:37 -0400

    Added test for Matern32

--- tests/testthat/test_kernel_model.R
+++ tests/testthat/test_kernel_model.R
@@ -80,20 +80,43 @@ test_that("kernel_Exponential works", {
   y <- f(x) #sin(2*pi*x) #+ rnorm(n,0,1e-1)
   gp <- GauPro_kernel_model$new(X=x, Z=y, kernel=Exponential$new(1), parallel=FALSE, verbose=10, nug.est=T)
 
-  expect_equal(gp$kernel$beta, 0.1806493, tolerance=.01)
-  expect_equal(gp$kernel$s2, 0.3809001, tolerance=.01)
-  expect_equal(gp$nug, 0.001037888, tolerance=.0001)
+  expect_equal(gp$kernel$beta, 0.430569, tolerance=.01)
+  expect_equal(gp$kernel$s2, 0.3158615, tolerance=.01)
+  expect_equal(gp$nug, 2.846939e-11, tolerance=.0001)
   expect_equal(
     # numDeriv::grad(func = function(x) {gp$deviance(params=x[1:2], nuglog=x[3])}, x=c(-.7,.227, -3.66)),
-    c(-20.509641,  11.177885,  -1.592343),
+    c(6.4639755,  16.3037264,  0.3593728),
     gp$deviance_grad(params = c(-.7,.227), nug.update=T, nuglog=-3.66),
     tol=.001
   )
   expect_equal(
     # numDeriv::grad(func = function(x) {gp$deviance(params=x[1:2], nuglog=x[3])}, x=c(2.5,-.2, -5)),
-    c(3847404.2, -2317793.3, -1894093.7),
+    c(13.011660214, 28.946608363, 0.000535044),
     gp$deviance_grad(params = c(2.5,-.2), nug.update=T, nuglog = -5),
     tol=100
   )
 })
+test_that("kernel_Matern32 works", {
+  set.seed(0)
+  n <- 20
+  x <- matrix(seq(0,1,length.out = n), ncol=1)
+  f <- Vectorize(function(x) {sin(2*pi*x) + .001*sin(8*pi*x) +rnorm(1,0,.03)})
+  y <- f(x) #sin(2*pi*x) #+ rnorm(n,0,1e-1)
+  gp <- GauPro_kernel_model$new(X=x, Z=y, kernel=Matern32$new(1), parallel=FALSE, verbose=10, nug.est=T)
 
+  expect_equal(gp$kernel$beta, 0.4609827, tolerance=.01)
+  expect_equal(gp$kernel$s2, 1.083443, tolerance=.01)
+  expect_equal(log(gp$nug,10), -11.37841, tolerance=.1)
+  expect_equal(
+    # numDeriv::grad(func = function(x) {gp$deviance(params=x[1:2], nuglog=x[3])}, x=c(-.7,.227, -3.66)),
+    c(-877.52290, -643.49553, -30.76981),
+    gp$deviance_grad(params = c(-.7,.227), nug.update=T, nuglog=-3.66),
+    tol=.001
+  )
+  expect_equal(
+    # numDeriv::grad(func = function(x) {gp$deviance(params=x[1:2], nuglog=x[3])}, x=c(2.5,-.2, -5)),
+    c(2.393571e+01, 3.066583e+01, 7.917006e-04),
+    gp$deviance_grad(params = c(2.5,-.2), nug.update=T, nuglog = -5),
+    tol=100
+  )
+})
