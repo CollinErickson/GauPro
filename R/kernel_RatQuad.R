246b4ffe1414144b9f7bc87e99b05649006d6b06
Collin Erickson <collinberickson@gmail.com>
2017-07-26 21:12:05 -0400

    Added RatQuad, works but alpha needs to be on log scale

---
+++ R/kernel_RatQuad.R
@@ -0,0 +1,207 @@
+# Kernels should implement:
+# k kernel function for two vectors
+# update_params
+# get_optim_functions: return optim.func, optim.grad, optim.fngr
+# param_optim_lower - lower bound of params
+# param_optim_upper - upper
+# param_optim_start - current param values
+# param_optim_start0 - some central param values that can be used for optimization restarts
+# param_optim_jitter - how to jitter params in optimization
+
+# Suggested
+# deviance
+# deviance_grad
+# deviance_fngr
+# grad
+
+
+
+#' Matern 5/2 Kernel R6 class
+#'
+#' @docType class
+#' @importFrom R6 R6Class
+#' @export
+#' @useDynLib GauPro, .registration = TRUE
+#' @importFrom Rcpp evalCpp
+#' @importFrom stats optim
+#' @keywords data, kriging, Gaussian process, regression
+#' @return Object of \code{\link{R6Class}} with methods for fitting GP model.
+#' @format \code{\link{R6Class}} object.
+#' @examples
+#' k1 <- RatQuad$new(beta=0)
+RatQuad <- R6::R6Class(classname = "GauPro_kernel_RatQuad",
+  inherit = GauPro_kernel_beta,
+  public = list(
+    # beta = NULL,
+    # beta_lower = NULL,
+    # beta_upper = NULL,
+    # beta_length = NULL,
+    # s2 = NULL, # variance coefficient to scale correlation matrix to covariance
+    # logs2 = NULL,
+    # logs2_lower = NULL,
+    # logs2_upper = NULL,
+    alpha = NULL,
+    alpha_lower = NULL,
+    alpha_upper = NULL,
+    alpha_est = NULL,
+    initialize = function(beta, alpha, s2=1,
+                          beta_lower=-8, beta_upper=6, beta_est=TRUE,
+                          alpha_lower=0, alpha_upper=Inf, alpha_est=TRUE,
+                          s2_lower=1e-8, s2_upper=1e8, s2_est=TRUE
+    ) {
+      super$initialize(beta=beta, s2=s2, beta_lower=beta_lower,
+                       beta_upper=beta_upper, beta_est=beta_est,
+                       s2_lower=s2_lower,s2_upper=s2_upper, s2_est=s2_est)
+      self$alpha <- alpha
+      self$alpha_lower <- alpha_lower
+      self$alpha_upper <- alpha_upper
+      self$alpha_est <- alpha_est
+
+    },
+    k = function(x, y=NULL, beta=self$beta, alpha=self$alpha, s2=self$s2, params=NULL) {#browser()
+      if (!is.null(params)) {
+        lenpar <- length(params)
+        beta <- params[1:(lenpar-2)]
+        alpha <- params[lenpar-1]
+        logs2 <- params[lenpar]
+        s2 <- 10^logs2
+      } else {#browser()
+        if (is.null(beta)) {beta <- self$beta}
+        if (is.null(alpha)) {alpha <- self$alpha}
+        if (is.null(s2)) {s2 <- self$s2}
+      }
+      theta <- 10^beta
+      if (is.null(y)) {
+        if (is.matrix(x)) {#browser()
+          # cgmtry <- try(val <- s2 * corr_gauss_matrix_symC(x, theta))
+          val <- outer(1:nrow(x), 1:nrow(x),
+                       Vectorize(function(i,j){
+                         self$kone(x[i,],x[j,],theta=theta, alpha=alpha, s2=s2)
+                       }))
+          # if (inherits(cgmtry,"try-error")) {browser()}
+          return(val)
+        } else {
+          return(s2 * 1)
+        }
+      }
+      if (is.matrix(x) & is.matrix(y)) {
+        # s2 * corr_gauss_matrixC(x, y, theta)
+        outer(1:nrow(x), 1:nrow(y), Vectorize(function(i,j){self$kone(x[i,],y[j,],theta=theta, alpha=alpha, s2=s2)}))
+      } else if (is.matrix(x) & !is.matrix(y)) {
+        # s2 * corr_gauss_matrixvecC(x, y, theta)
+        apply(x, 1, function(xx) {self$kone(xx, y, theta=theta, alpha=alpha, s2=s2)})
+      } else if (is.matrix(y)) {
+        # s2 * corr_gauss_matrixvecC(y, x, theta)
+        apply(y, 1, function(yy) {self$kone(yy, x, theta=theta, alpha=alpha, s2=s2)})
+      } else {
+        self$kone(x, y, theta=theta, alpha=alpha, s2=s2)
+      }
+    },
+    kone = function(x, y, beta, theta, alpha, s2) {
+      if (missing(theta)) {theta <- 10^beta}
+      # t1 <- self$sqrt
+      r2 <- sum(theta * (x-y)^2)
+      s2 * (1 + r2 / alpha) ^ -alpha
+    },
+    dC_dparams = function(params=NULL, X, C_nonug, C, nug) {#browser(text = "Make sure all in one list")
+      n <- nrow(X)
+      if (is.null(params)) {params <- c(self$beta, self$alpha, self$logs2)}
+      if (missing(C_nonug)) { # Assume C missing too, must have nug
+        C_nonug <- self$k(x=X, params=params)
+        C <- C_nonug + diag(nug*10^params[length(params)], nrow(C_nonug))
+      }
+      lenparams <- length(params)
+      beta <- params[1:(lenparams - 2)]
+      alpha <- params[lenparams-1]
+      theta <- 10^beta
+      log10 <- log(10)
+      logs2 <- params[lenparams]
+      s2 <- 10 ^ logs2
+      dC_dparams <- array(dim=c(lenparams, n, n))
+      dC_dparams[lenparams,,] <- C * log10
+      for (k in 1:length(beta)) {
+        for (i in seq(1, n-1, 1)) {
+          for (j in seq(i+1, n, 1)) {
+            r2 <- sum(theta * (X[i,]-X[j,])^2)
+            t1 <- 1 + r2 / alpha
+            dC_dparams[k,i,j] <- -C[i,j] * (X[i,k] - X[j,k])^2  / t1 * theta[k] * log10   #s2 * (1+t1) * exp(-t1) *-dt1dbk + s2 * dt1dbk * exp(-t1)
+            dC_dparams[k,j,i] <- dC_dparams[k,i,j]
+          }
+        }
+        for (i in seq(1, n, 1)) { # Get diagonal set to zero
+          dC_dparams[k,i,i] <- 0
+        }
+      }
+      # Grad for alpha
+      for (i in seq(1, n-1, 1)) {
+        for (j in seq(i+1, n, 1)) {
+          r2 <- sum(theta * (X[i,]-X[j,])^2)
+          t1 <- 1 + r2 / alpha
+          dC_dparams[lenparams-1, i,j] <- C[i,j] * (- log(t1) + r2 / alpha / t1)
+          dC_dparams[lenparams-1, j,i] <- dC_dparams[lenparams-1, i,j]
+        }
+      }
+      for (i in seq(1, n, 1)) {
+        dC_dparams[lenparams-1, i,i] <- 0
+      }
+      return(dC_dparams)
+    },
+    param_optim_start = function(jitter=F, y, beta_est=self$beta_est,
+                                 alpha_est=self$alpha_est, s2_est=self$s2_est) {
+      # Use current values for theta, partial MLE for s2
+      # vec <- c(log(self$theta, 10), log(sum((y - mu) * solve(R, y - mu)) / n), 10)
+      if (beta_est) {vec <- c(self$beta)} else {vec <- c()}
+      if (alpha_est) {vec <- c(vec, self$alpha)} else {}
+      if (s2_est) {vec <- c(vec, self$s2)} else {}
+      if (jitter && beta_est) {
+        # vec <- vec + c(self$beta_optim_jitter,  0)
+        vec[1:length(self$beta)] = vec[1:length(self$beta)] + rnorm(length(self$beta), 0, 1)
+      }
+      vec
+    },
+    param_optim_start0 = function(jitter=F, y, beta_est=self$beta_est,
+                                  alpha_est=self$alpha_est, s2_est=self$s2_est) {
+      # Use 0 for theta, partial MLE for s2
+      # vec <- c(rep(0, length(self$theta)), log(sum((y - mu) * solve(R, y - mu)) / n), 10)
+      if (beta_est) {vec <- rep(0, self$beta_length)} else {vec <- c()}
+      if (alpha_est) {vec <- c(vec, 2)} else {}
+      if (s2_est) {vec <- c(vec, 0)} else {}
+      if (jitter && beta_est) {
+        vec[1:length(self$beta)] = vec[1:length(self$beta)] + rnorm(length(self$beta), 0, 1)
+      }
+      vec
+    },
+    param_optim_lower = function(beta_est=self$beta_est,
+                                 alpha_est=self$alpha_est,
+                                 s2_est=self$s2_est) {
+      # c(self$beta_lower, self$logs2_lower)
+      if (beta_est) {vec <- c(self$beta_lower)} else {vec <- c()}
+      if (alpha_est) {vec <- c(vec, self$alpha_lower)} else {}
+      if (s2_est) {vec <- c(vec, self$logs2_lower)} else {}
+      vec
+    },
+    param_optim_upper = function(beta_est=self$beta_est,
+                                 alpha_est=self$alpha_est,
+                                 s2_est=self$s2_est) {
+      # c(self$beta_upper, self$logs2_upper)
+      if (beta_est) {vec <- c(self$beta_upper)} else {vec <- c()}
+      if (alpha_est) {vec <- c(vec, self$alpha_upper)} else {}
+      if (s2_est) {vec <- c(vec, self$logs2_upper)} else {}
+      vec
+    },
+    set_params_from_optim = function(optim_out, beta_est=self$beta_est,
+                                     alpha_est=self$alpha_est, s2_est=self$s2_est) {
+      loo <- length(optim_out)
+      if (beta_est) {
+        self$beta <- optim_out[1:(self$beta_length)]
+      }
+      if (alpha_est) {
+        self$alpha <- optim_out[(1 + beta_est * self$beta_length)]
+      }
+      if (s2_est) {
+        self$logs2 <- optim_out[loo]
+        self$s2 <- 10 ^ self$logs2
+      }
+    }
+  )
+)
---
+++ scratch/scratch_kernel_RatQuad.R
@@ -0,0 +1,65 @@
+# Check numerically that gradient is correct for 1D
+# For RatQuad kernel
+set.seed(0)
+n <- 20
+x <- matrix(seq(0,1,length.out = n), ncol=1)
+f <- Vectorize(function(x) {sin(2*pi*x) + .5*sin(4*pi*x) +rnorm(1,0,.3)})
+y <- f(x) #sin(2*pi*x) #+ rnorm(n,0,1e-1)
+gp <- GauPro_kernel_model$new(X=x, Z=y, kernel=RatQuad$new(1, 1.5), parallel=FALSE, verbose=10, nug.est=T)
+gp$cool1Dplot()
+numDeriv::grad(func = function(x)gp$deviance(params=x[1:3], nuglog=x[4]), x=c(2,9, .2, -4))
+gp$deviance_grad(params = c(2,9, .2), nug.update=T, nuglog=-4)
+numDeriv::grad(func = function(x)gp$deviance(params=x[1:3], nuglog=x[4]), x=c(gp$kernel$beta, gp$kernel$alpha, gp$kernel$logs2, log(gp$nug,10)))
+gp$deviance_grad(params = c(gp$kernel$beta, gp$kernel$alpha, gp$kernel$logs2), nug.update=T, nuglog=log(gp$nug,10))
+
+# Check dC_dtheta
+m1 <- (gp$kernel$k(gp$X, beta=1,alpha=5) - gp$kernel$k(gp$X, beta=1-1e-6, alpha=5)) / 1e-6
+C_nonug <- gp$kernel$k(gp$X, beta=1, alpha=5)
+C <- C_nonug + gp$kernel$s2 * diag(gp$nug, nrow(C_nonug))
+m2 <- gp$kernel$dC_dparams(params = c(1, 5, 1), X = gp$X, C = C, C_nonug = C_nonug)[1,,]
+summary(c(m1-m2))# %>% summary
+plot(m1, m2)
+
+gp$deviance_grad()
+dsign <- 1
+mm1 <- gp$kernel$dC_dparams(C = C, C_nonug = C_nonug, X=gp$X)[[1]]
+dsign <- -1
+mm2 <- gp$kernel$dC_dparams(C = C, C_nonug = C_nonug, X=gp$X)[[1]]
+plot(c(mm1[[1]]), c(mm2[[1]]))
+
+
+# Check dC_dlogs2
+beta <- gp$kernel$beta
+s2 <- gp$kernel$s2+.2
+nug <- gp$nug
+eps <- 1e-6
+m1 <- (gp$kernel$k(gp$X, beta=beta, s2=s2+eps) - gp$kernel$k(gp$X, beta=beta, s2=s2-eps)) / eps / 2
+C_nonug <- gp$kernel$k(gp$X, beta=beta, s2=s2)
+C <- C_nonug + s2 * diag(nug, nrow(C_nonug))
+m2 <- gp$kernel$dC_dparams(params = log(c(beta, s2),10), X = gp$X, C = C, C_nonug = C)[[1]][[2]]
+c(m1 * s2 * log(10) -m2) %>% summary
+plot(c(m1 * s2 * log(10)), c(m2))
+
+
+# Check C_dC_dparams
+params <- c(1.2,.8)
+nug <- .001
+gp$deviance(params=params, nug=nug)
+gp$deviance_grad(params=params, nug=nug, nug.update=T)
+gp$deviance_fngr(params=params, nug=nug, nug.update=T)
+microbenchmark::microbenchmark(sep={gp$deviance(params=params, nug=nug);gp$deviance_grad(params=params, nug=nug, nug.update=T)}, fngr=gp$deviance_fngr(params=params, nug=nug, nug.update=T))
+
+
+
+
+# Check 2D
+set.seed(0)
+n <- 30
+x <- lhs::maximinLHS(n=n, k=2)
+f <- function(x) {sin(2*pi*x[1]) + .5*sin(4*pi*x[1]) +rnorm(1,0,.03) + x[2]^2}
+y <- apply(x, 1, f) #f(x) #sin(2*pi*x) #+ rnorm(n,0,1e-1)
+gp <- GauPro_kernel_model$new(X=x, Z=y, kernel=RatQuad$new(c(1, 1)), parallel=FALSE, verbose=10, nug.est=T)
+ContourFunctions::cf(gp$predict, pts=x, batchmax=Inf)
+ContourFunctions::cf(f, pts=x)
+numDeriv::grad(func = function(x)gp$deviance(params = x[1:3], nuglog=x[4]), x=c(1,1, 1, -4))
+gp$deviance_grad(params = c(1,1,1), nug.update=T, nuglog=-4)
