45d0ebeb5a32d9ee8ca269d7dc716c0316463584
Collin Erickson <collinberickson@gmail.com>
2017-07-27 22:22:26 -0400

    Created trend R6 class

---
+++ R/trend_base.R
@@ -0,0 +1,34 @@
+# Trend functions should implement:
+# mu prediction for new matrix
+# update_params
+# get_optim_functions: return optim.func, optim.grad, optim.fngr
+# param_optim_lower - lower bound of params
+# param_optim_upper - upper
+# param_optim_start - current param values
+# param_optim_start0 - some central param values that can be used for optimization restarts
+# param_optim_jitter - how to jitter params in optimization
+
+
+
+
+#' Trend R6 class
+#'
+#' @docType class
+#' @importFrom R6 R6Class
+#' @export
+#' @useDynLib GauPro, .registration = TRUE
+#' @importFrom Rcpp evalCpp
+#' @importFrom stats optim
+#' @keywords data, kriging, Gaussian process, regression
+#' @return Object of \code{\link{R6Class}} with methods for fitting GP model.
+#' @format \code{\link{R6Class}} object.
+#' @examples
+#' k <- GauPro_trend$new()
+GauPro_trend <- R6::R6Class(classname = "GauPro_trend",
+                             public = list(
+                               # Gradient is -2 * t(yminusmu) %*% Siginv %*% du/db
+                             ),
+                             private = list(
+
+                             )
+)
---
+++ R/trend_constant.R
@@ -0,0 +1,51 @@
+# Trend functions should implement:
+# mu prediction for new matrix
+# update_params
+# get_optim_functions: return optim.func, optim.grad, optim.fngr
+# param_optim_lower - lower bound of params
+# param_optim_upper - upper
+# param_optim_start - current param values
+# param_optim_start0 - some central param values that can be used for optimization restarts
+# param_optim_jitter - how to jitter params in optimization
+
+
+
+
+#' Trend R6 class
+#'
+#' @docType class
+#' @importFrom R6 R6Class
+#' @export
+#' @useDynLib GauPro, .registration = TRUE
+#' @importFrom Rcpp evalCpp
+#' @importFrom stats optim
+#' @keywords data, kriging, Gaussian process, regression
+#' @return Object of \code{\link{R6Class}} with methods for fitting GP model.
+#' @format \code{\link{R6Class}} object.
+#' @examples
+#' t1 <- trend_c$new()
+trend_c <- R6::R6Class(classname = "GauPro_trend",
+  public = list(
+    m = NULL,
+    m_lower = NULL,
+    m_upper = NULL,
+    m_est = NULL,
+    initialize = function(m = 0, m_lower=-Inf, m_upper=Inf, m_est=TRUE) {
+      self$m <- m
+      self$m_lower <- m_lower
+      self$m_upper <- m_upper
+      self$m_est <- m_est
+    },
+    Z = function(X, m=self$m) {
+      if (is.matrix(X)) {
+        matrix(m, nrow=nrow(X), ncol=1)
+      } else { # If a vector then just a single value
+        m
+      }
+    },
+    dZ_dparams = function(X, m=m$est) {
+      # Gradient is -2 * t(yminusmu) %*% Siginv %*% du/db
+      matrix(1, nrow=nrow(X), ncol=1)
+    }
+  )
+)
