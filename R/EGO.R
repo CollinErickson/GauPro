898d83b74a52278ba8a0ccc776152a29cbfa0f85
Collin Erickson <collinberickson@gmail.com>
2017-08-19 10:19:24 -0400

    Working on EGO

--- R/EGO.R
+++ R/EGO.R
@@ -2,10 +2,20 @@ EGO <- R6::R6Class(
   classname = "EGO",
   public = list(
     gp = NULL,
-    initialize = function(X, Z, n) {
+    func = NULL,
+    n0 = NULL,
+    n = NULL,
+    initialize = function(func, X, Z, n0, n) {
       self$gp <- GauPro_kernel_model$new(X=X, Z=Z, kernel=kernel)
+      self$func <- func
+      self$n0 <- n0
       self$n <- n
+
+      self$initial_run()
       self$run()
+    },
+    initial_run = function() {
+      X <- lhs::randomLHS(n=n0, k=)
     },
     run = function() {
       for (i in 1:n) {
