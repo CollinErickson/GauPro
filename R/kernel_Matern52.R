eb247928a9f1461adb139feb42c46a0c4829b2ca
Collin Erickson <collinberickson@gmail.com>
2017-08-31 17:13:18 -0500

    Cleaned code

--- R/kernel_Matern52.R
+++ R/kernel_Matern52.R
@@ -1,21 +1,3 @@
-# Kernels should implement:
-# k kernel function for two vectors
-# update_params
-# get_optim_functions: return optim.func, optim.grad, optim.fngr
-# param_optim_lower - lower bound of params
-# param_optim_upper - upper
-# param_optim_start - current param values
-# param_optim_start0 - some central param values that can be used for optimization restarts
-# param_optim_jitter - how to jitter params in optimization
-
-# Suggested
-# deviance
-# deviance_grad
-# deviance_fngr
-# grad
-
-
-
 #' Matern 5/2 Kernel R6 class
 #'
 #' @docType class
