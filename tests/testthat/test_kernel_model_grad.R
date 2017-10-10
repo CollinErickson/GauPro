1205373d4e298767ffd08253c873bb586f99668f
Collin Erickson <collinberickson@gmail.com>
2017-10-10 16:07:05 -0500

    Fixed error

--- tests/testthat/test_kernel_model_grad.R
+++ tests/testthat/test_kernel_model_grad.R
@@ -62,7 +62,7 @@ test_that("grad_norm2 for Gaussian", { # only implemented for Gaussian now
   # Check 2D
   set.seed(0)
   n <- 30
-  x <- lhs::maximinLHS(n=n, k=2)
+  x <- matrix(runif(2*n), ncol=2)
   f <- function(x) {sin(2*pi*x[1]) + .5*sin(4*pi*x[1]) +rnorm(1,0,.03) + x[2]^2}
   y <- apply(x, 1, f) #f(x) #sin(2*pi*x) #+ rnorm(n,0,1e-1)
   gp <- GauPro_kernel_model$new(X=x, Z=y, kernel=Gaussian$new(beta=c(.966, -.64),s2=10^.4848), parallel=FALSE, verbose=10, nug.est=F, nug=0.0001721, param.est=F)
