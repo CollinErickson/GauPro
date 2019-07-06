205cce3fbc7bd6092df278d569c44d7060b4a694
Collin Erickson <collinberickson@gmail.com>
2019-07-05 22:17:32 -0400

    rbf scratch

--- scratch/scratch_RBF.R
+++ scratch/scratch_RBF.R
@@ -10,7 +10,7 @@ RBFfit <- function(X, Y) {#browser()
   centers <- X
   n <- nrow(X)
   self$BasisFunc <- function(x,y) {
-    sqrt(abs((x - y)^2))
+    sqrt(sum((x - y)^2))
   }
   Phi <- outer(1:n, 1:n, Vectorize(
     function(i,j) {
@@ -71,10 +71,11 @@ points(x, y)
 
 AugRBFfit <- function(X, Y) {#browser()
   self <- list()
-  centers <- X
+  # centers <- X
   n <- nrow(X)
   self$BasisFunc <- function(x,y) {
-    sqrt(abs((x - y)^2))
+    sqrt(sum((x - y)^2))^3
+    # exp(-sum((x - y)^2)/2/.5^2)
   }
   Phi <- outer(1:n, 1:n, Vectorize(
     function(i,j) {
@@ -89,10 +90,10 @@ AugRBFfit <- function(X, Y) {#browser()
   for (ki in 1:k) {
     P[,1+ki*d - d + 1:d] <- X ^ ki
   }
-  AugPhi <- rbind(cbind(Phi, P), cbind(t(P), matrix(0,k+1,k+1)))
+  AugPhi <- rbind(cbind(Phi, P), cbind(t(P), matrix(0,k*d+1,k*d+1)))
   AugPhiinv_Y <- solve(AugPhi, rbind(Y, matrix(0,k*d+1,1)))
   theta <- AugPhiinv_Y[1:n]
-  mu <- AugPhiinv_Y[(n+1):(n+k+1)]
+  mu <- AugPhiinv_Y[(n+1):(n+1 + k*d)]
 
   self$X <- X
   self$Y <- Y
@@ -107,19 +108,20 @@ AugRBFfit <- function(X, Y) {#browser()
 AugRBFpred <- function(self, xp) {#browser()
   print("using augmented")
   n <- nrow(self$X)
+  d <- ncol(self$X)
   np <- nrow(xp)
-  phi <- outer(1:n, 1:np, function(i, ip) {
+  phi <- outer(1:n, 1:np, Vectorize(function(i, ip) {#browser()
     # sqrt(abs((self$X[i,] - xp[ip,])^2))
     self$BasisFunc(self$X[i,], xp[ip,])
-  })
+  }))
   # browser()
-  Ypredmean <- t(phi) %*% self$theta + rowSums(matrix(self$mu, nrow(xp), self$k+1, byrow=T) * do.call(cbind, lapply(0:self$k, function(ki) xp^ki)))
+  Ypredmean <- t(phi) %*% self$theta + rowSums(matrix(self$mu, nrow(xp), d*self$k+1, byrow=T) * do.call(cbind, lapply(0:self$k, function(ki) {if (ki==0) {matrix(1,nrow(xp),1)} else {xp^ki}})))
 
   # Get var
   Ypredvar <- self$BasisFunc(self$X[1,], self$X[1,]) - t(phi) %*% solve(self$Phi, phi)
 
   # Return mean and var
-  list(mean=Ypredmean, var=diag(-Ypredvar), cov=Ypredvar)
+  list(mean=Ypredmean, var=diag(Ypredvar), cov=Ypredvar)
 }
 
 
@@ -136,7 +138,30 @@ AugRBFpred(arb1, xp)
 
 xl <- seq(0,1,l=100)
 apr <- AugRBFpred(arb1, as.matrix(xl))
-plot(xl, apr$me+2*sqrt(apr$v), type='l', col=3, ylim=c(min(apr$me-2*sqrt(apr$v)), max(apr$me+2*sqrt(apr$v))))
-points(xl, apr$me-2*sqrt(apr$v), type='l', col=3)
+plot(xl, apr$me+2*sqrt(apr$v+1e-12), type='l', col=3, ylim=c(min(apr$me-2*sqrt(apr$v+1e-12)), max(apr$me+2*sqrt(apr$v+1e-12))))
+points(xl, apr$me-2*sqrt(apr$v+1e-12), type='l', col=3)
 points(xl, apr$me, type='l')
 points(x, y)
+
+
+
+
+
+d <- 8
+n <- 300*3
+x <- matrix(runif(d*n), ncol=d)
+f <- TestFunctions::borehole #function(x) {cos(x[1]^1.9)}
+y <- as.matrix(apply(x, 1, f))
+arb1 <- AugRBFfit(X=x, Y = y)
+np <- 300
+xp <- matrix(runif(d*np), ncol=d)
+p1 <- AugRBFpred(arb1, xp)
+predpoly <- function(self, xp) {
+  rowSums(matrix(self$mu, nrow(xp), d*self$k+1, byrow=T) *
+            do.call(cbind, lapply(0:self$k, function(ki) {if (ki==0) {matrix(1,nrow(xp),1)} else {xp^ki}})))
+}
+p2 <- predpoly(arb1, xp)
+yp <- as.matrix(apply(xp, 1, f))
+SGGP::valplot(p1$mean, p1$var, yp)
+plot(yp, p1$mean);abline(a=0,b=1, col=3)
+points(yp, p2, col=4)#;abline(a=0,b=1, col=4)
